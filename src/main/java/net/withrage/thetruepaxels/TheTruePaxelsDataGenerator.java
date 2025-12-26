fa183ae0ac1daba018016af1bce55da3a1745f4a
justerik <sebo.erik@gmail.com>
2025-12-26 19:31:17 +0100

    Initial commit for fabric 1.20.4 port

---
+++ src/main/java/net/withrage/thetruepaxels/TheTruePaxelsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.withrage.thetruepaxels;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheTruePaxelsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
