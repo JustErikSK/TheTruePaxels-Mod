7611cbfc3f3ffb583d24cd6cfdbd1d54e198e0f0
justerik <sebo.erik@gmail.com>
2026-02-27 23:31:49 +0100

    ported v1.1.0 to 1.20.4

--- src/main/java/net/withrage/thetruepaxels/item/custom/ModToolMaterial.java
+++ src/main/java/net/withrage/thetruepaxels/item/custom/ModToolMaterial.java
@@ -1,7 +1,9 @@
 package net.withrage.thetruepaxels.item.custom;
 
+import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.ToolMaterials;
+import net.minecraft.recipe.Ingredient;
 
 public class ModToolMaterial {
     public static final ToolMaterial WOOD = ToolMaterials.WOOD;
@@ -10,4 +12,36 @@ public class ModToolMaterial {
     public static final ToolMaterial IRON = ToolMaterials.IRON;
     public static final ToolMaterial DIAMOND = ToolMaterials.DIAMOND;
     public static final ToolMaterial NETHERITE = ToolMaterials.NETHERITE;
+
+    public static final ToolMaterial COPPER = new ToolMaterial() {
+        @Override
+        public int getDurability() {
+            return 262;
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier() {
+            return 5.0F;
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return 2.0F;
+        }
+
+        @Override
+        public int getMiningLevel() {
+            return 2;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return 12;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return Ingredient.ofItems(Items.COPPER_INGOT);
+        }
+    };
 }
